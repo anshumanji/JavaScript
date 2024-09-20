a044a08a36f72dc86e5ca30980bd55bc1b155158
Anshuman Soni <125365257+anshumanji@users.noreply.github.com>
2024-09-20 04:41:08 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+console.log("hello");
+
+var a = "hello";
+const z = "Hello";
+x = "hello";
+let y = "Hello";
+console.table([a,z,x,y])
+
+
+// prefer not to use var because of issue in block scope and functional scope
