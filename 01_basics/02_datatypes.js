27abb6a9bc39d7774184d5545fb45bd771a09e8b
Anshuman Soni <125365257+anshumanji@users.noreply.github.com>
2024-09-20 04:54:06 +0000

    dataypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict"; 
+// treat all js code as newer version
+
+// alert(3+3)  we are using node js not browser
+
+let name = "Anshuman" //string
+let age  = 18 //number 2 to power 53
+let isLoggedin = true //boolean
+// null => standalone value
+//symbol => unique
