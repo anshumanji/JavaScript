4bcb1f9a1b09de05b51de86d4a0bfb4f09504e54
Anshuman Soni <125365257+anshumanji@users.noreply.github.com>
2024-09-24 05:31:43 +0000

    typeconversion

---
+++ 01_basics/03_conversionoper.js
@@ -0,0 +1,31 @@
+// Coversion Opertion
+//--------------------String to Number--------------
+
+let con = ""
+let num = Number(con)
+// op:0
+
+let alstring = "33abc"
+let numstring = Number(alstring)
+// op:NaN
+
+
+let boolstring = true
+let boolNum = Number(boolstring)
+// op:1
+
+let boolfastring = false
+let boolfNum = Number(boolfstring)
+// op:0
+
+
+//--------------------Number to string--------------
+let stringnum = 33;
+let string = String(stringnum)
+// op:33
+
+//number to boolean
+let numbool = 1;
+let bool = Boolean(numbool)
+// op:true  similarly if 0 then false
+
