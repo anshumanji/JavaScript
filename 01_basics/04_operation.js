c0461bfcdbbbc4a84bc256c1dca7f57b0c8e5f6b
Anshuman Soni <125365257+anshumanji@users.noreply.github.com>
2024-09-24 06:30:02 +0000

    operation

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,25 @@
+//operation
+
+let value = 33
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+console.log(1+2+"2"); //op 32
+console.log('1'+'2'+2); //op 112
+
+console.log(+true); //op 1
+//console.log(true+); //op error
+
+
+
+
+
+
