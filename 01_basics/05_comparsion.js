e66259005e04d9a7ee9c5f22a506e74a3b811f8d
Anshuman Soni <125365257+anshumanji@users.noreply.github.com>
2024-09-30 05:23:00 +0000

    comparion

--- 01_basics/05_comparsion.js
+++ 01_basics/05_comparsion.js
@@ -1,2 +1,9 @@
 console.log("">1);
 console.log("2">1);
+
+console.log(null>0);
+console.log(null===0);
+console.log(null>=0);
+
+
+//the reason is that equality check comparison converts null to a number treating it as zero 
