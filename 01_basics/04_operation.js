2364c031eaf36de966c17ce7ade68fab9c981d33
Anshuman Soni <125365257+anshumanji@users.noreply.github.com>
2024-10-06 08:25:51 +0000

    operation done

--- 01_basics/04_operation.js
+++ 01_basics/04_operation.js
@@ -17,6 +17,16 @@ console.log('1'+'2'+2); //op 112
 
 console.log(+true); //op 1
 //console.log(true+); //op error
+console.log(+""); //conversion to integer using '+'
+
+//prefix increment and decre
+let gameCounter = 100
+console.log(gameCounter++);
+console.log(gameCounter++);
+console.log(gameCounter);
+
+
+
 
 
 
