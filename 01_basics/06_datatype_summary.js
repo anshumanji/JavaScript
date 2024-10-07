a7e76dd161b18b23f157c6bca32863d29e76e43d
Anshuman Soni <125365257+anshumanji@users.noreply.github.com>
2024-10-07 06:23:41 +0000

    datatype_summary

---
+++ 01_basics/06_datatype_summary.js
@@ -0,0 +1,40 @@
+const score = 100 //Number
+const scorefloat = 10.3 //NUmber
+
+const outSideTemp = null
+
+// symbol
+const id = Symbol("123")
+const EmpId = Symbol("123")
+
+console.log(id === EmpId); // symbol always return unique
+
+let s = Symbol('+')
+let b = Symbol('+')
+console.log(s==b) //op false because symbol assign unique value
+
+//all primitive dt are call by value
+// non-primitive are call by reference
+
+
+// #Non-Primitive DT
+const arr = ["Hello", "1", "ram"]
+console.log(arr)
+console.log(typeof arr)
+
+
+//2.objects 
+object are keyvalue piar
+let myobj= {
+    name: "Anshu",
+    age : 20
+}
+console.log(myobj)
+console.log(typeof myobj)
+
+// const myfunction  = function(){
+//     console.log("Hello World");
+// }
+
+// console.log(myfunction);
+
