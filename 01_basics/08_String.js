591b90545c7e7476a026105a9dd324f004eef470
Anshuman Soni <125365257+anshumanji@users.noreply.github.com>
2024-10-07 08:25:45 +0000

    string method

---
+++ 01_basics/08_String.js
@@ -0,0 +1,36 @@
+ const name = 'hitesh'
+ const repoCount  = 50
+
+//  console.log(name+ repoCount + " Value");
+
+//always try to use `` for displaying
+// ${}=> string interpulation
+
+
+// console.log(`Hello my Name is ${name.trimEnd()} and my repoCount is ${repoCount}`);
+
+
+
+// console.log(name[4])
+// console.log(name.charAt(4))
+// console.log(name.length)
+console.log(name.indexOf('t'));
+
+
+console.log(name.substring(0,4))
+//substring ignores negative value and start with 0
+
+console.log(name.slice(-6,4))
+//slice accept neg value
+const anotherString = "      hitesh        "
+console.log(anotherString)
+console.log(anotherString.trim())
+
+const url = "https://anshuman.com/anshuman%20soni"
+
+console.log(url.replace('%20','-'))
+
+
+// console.log(name.__proto__)
+
+
