cc7e9790e192a8cff6815e832c7c870a5d71a278
Anshuman Soni <125365257+anshumanji@users.noreply.github.com>
2024-10-07 07:38:38 +0000

    stack and heap memory

---
+++ 01_basics/07_memory.js
@@ -0,0 +1,24 @@
+//stack =>primitive dt
+//heap => non-primitive
+
+let myName = "Anshuman"
+
+let anotherName = myName
+
+anotherName ="hitesh"
+
+
+let userOne = {
+    email:"user@gmail.com",
+    salary:50000
+}
+
+
+let userTwo = userOne
+
+userTwo.email="hitesh@gmail.com"
+
+console.log(userOne);
+
+
+
